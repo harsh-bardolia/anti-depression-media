50377762c25ba14d7b49f4d18b7ac0c590bef41b
Harsh Bardolia <110019311+harsh-bardolia@users.noreply.github.com>
2022-11-18 10:52:31 +0530

    Update Banner.js

--- pages/components/Banner.js
+++ pages/components/Banner.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import banner from "/image-assets/banner.png";
+import Banner from "/image-assets/banner.png";
 import Quoto from "./Quoto";
 
 const Banner = () => {
@@ -8,7 +8,7 @@ const Banner = () => {
     <div className="dark:bg-gray-700 ">
       <div className="relative h-[300px] w-100vw -mt-2 sm:h-[360px] md:h-[400px] lg:h-[450px] xl:h-[500px] 2xl:h-[600px] ">
         <Image
-          src={banner}
+          src={Banner}
           layout="fill"
           objectFit="contain"
           className="md:-py-5 "
