d433b89a8694c81eb1b4ecdba06068d67f965824
harsh-bardolia <hbardolia2002@gmail.com>
2023-03-07 18:48:04 +0530

    Change Context API

--- Context/UIContext.js
+++ Context/UIContext.js
@@ -1,4 +1,5 @@
 import { useContext, createContext, useState } from "react";
+import useLocalStorage from "use-local-storage";
 
 const UIContext = createContext({});
 
@@ -8,7 +9,9 @@ export const useUI = () => {
 
 export const UIProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
-
+  const [x, setX] = useLocalStorage('Mode',false);
+  const [user, setUser] = useState(null);
+  
   const [navMenu, setNavMenu] = useState(false);
 
   const toggleNavMenu = () => {
@@ -16,13 +19,25 @@ export const UIProvider = ({ children }) => {
   };
 
   const toggleTheme = () => {
+    setX(!x);
     setTheme(theme === "light" ? "dark" : "light");
   };
 
+  const userInfo = () =>{
+    setUser({
+        name: user.displayName,
+        photoUrl: user.photoURL,
+      })
+  }
+
   const value = {
+    x,
+    user,
     theme,
     setTheme,
     navMenu,
+    userInfo,
+    setUser,
     setNavMenu,
     toggleNavMenu,
     toggleTheme,
